f4445d9cea06bf3ebcd3665aec911987895df317
Gianguido Sorà <g.sora4@gmail.com>
2016-02-26 21:00:40 +0100

    added a function to send messages, plus now the bot echoes what you write!

--- sysinfobot.go
+++ sysinfobot.go
@@ -6,22 +6,28 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
+var baseURL = ""
+
 func main() {
+	// check for presence and correctness of the configuration file
 	conf, err := support.CheckConfigFile()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	baseURL := "https://api.telegram.org/bot" + conf.BotToken + "/"
+	// set the base URL for api calls
+	baseURL = "https://api.telegram.org/bot" + conf.BotToken + "/"
 
-	//declare eventHook for method messageUpdate
+	// set the webhook as the configuration file says
 	_, err = http.PostForm(baseURL+"setWebhook", url.Values{"url": {conf.URL + ":" + conf.Port + "/" + conf.Endpoint}})
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// start the bot!
 	fmt.Println("--> Starting sysinfobot")
 	err = support.PrintBotInformations(conf.BotToken)
 	if err != nil {
@@ -32,7 +38,23 @@ func main() {
 	log.Fatal(http.ListenAndServeTLS(":"+conf.Port, conf.CertPath, conf.KeyPath, nil))
 }
 
+// handle the webhook data
 func endpointHandler(w http.ResponseWriter, r *http.Request) {
 	data := support.LoadJSONToTelegramObject(r.Body)
-	fmt.Println(data)
+	echoText := data.Message.Text
+	refChat := data.Message.Chat.ID
+	sendMessage(strconv.Itoa(refChat), echoText)
+
+}
+
+// simple function to send a message back to its chat
+func sendMessage(chatID string, text string) {
+	params := url.Values{}
+	params.Set("chat_id", chatID)
+	params.Set("text", text)
+
+	_, err := http.PostForm(baseURL+"sendMessage", params)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
